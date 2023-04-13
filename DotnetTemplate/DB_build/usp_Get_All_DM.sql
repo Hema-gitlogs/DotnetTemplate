f714d1a8284df61315476412b0cebac89f8ce39d
Hema-gitlogs <87534708+Hema-gitlogs@users.noreply.github.com>
2023-04-13 12:50:10 +0530

    Add files via upload

---
+++ DotnetTemplate/DB_build/usp_Get_All_DM.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE dbo.usp_Get_All_DM
+AS
+BEGIN
+SELECT 
+	U.InnovaId,
+	U.UserName,
+	U.UserEmail,
+	U.Designation,
+	UR.NAME AS UserRole,
+	U.IsActive
+FROM USERS U
+INNER JOIN UserRole UR ON U.userroleid=UR.Userroleid
+END
